3d07518e808778308f8f30787dd5db2b171d7a12
Sören König <soeren.koenig@zalando.de>
2014-04-22 00:59:52 +0200

    added note about rpm building from python

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,15 @@ import os
 from fabric.api import local, hide, execute
 from fabric.decorators import task
 
+
+@task
+def deps_list_centos(package):
+    # @TODO fpm seems to fail while building a rpm from python packages which has dependencies
+    # (see: https://github.com/jordansissel/fpm/issues/571), however - command for listing rpm
+    # dependencies: `rpm -qp --requires package.rpm`
+    pass
+
+
 @task
 def deps_list_debian(package):
     deps = []
@@ -14,11 +23,12 @@ def deps_list_debian(package):
         os.path.isfile(package) or abort('{0} doesn\'t exist.'.format(package))
         deps_string = local('dpkg --field {0} Depends'.format(package), capture=True)
         for dependency in deps_string.split(','):
-            dependency = dependency.replace('(', '').replace(')', '').replace(' ','')
-            if not ('>=' in dependency or '<=' in dependency) and dependency.count('=')  == 1:
+            dependency = dependency.replace('(', '').replace(')', '').replace(' ', '')
+            if not ('>=' in dependency or '<=' in dependency) and dependency.count('=') == 1:
                 dependency = dependency.replace('=', '==')
             deps.append(dependency)
     return deps
+
 
 @task
 def pypi_build(package_name):
@@ -29,10 +39,11 @@ def pypi_build(package_name):
     if package_name.startswith('zalando-'):
         pypi_uri = 'http://iftp.zalando.net/simple'
 
-    messages = local('sudo fpm -s python --python-pypi {0} -t deb --force "{1}"'.format(pypi_uri, package_name), capture=True)
+    messages = local('sudo fpm -s python --python-pypi {0} -t deb --force "{1}"'.format(pypi_uri, package_name),
+                     capture=True)
     for message in messages.split('\n'):
         if 'Created deb package' in message:
-            package = message.split(':path=>')[1].replace('"','').replace('}','')
+            package = message.split(':path=>')[1].replace('"', '').replace('}', '')
 
     if package is not None:
         package_dependencies = execute('deps_list_debian', package)
@@ -46,6 +57,7 @@ def pypi_build(package_name):
             # fpm prefixes packages and their dependencies with "python-" by default
             # when they are build from pypi, what makes sense for the destination packages.
             # We have to build the dependencies also from pypi, so remove the prefix here:
-            package_dependency = package_dependency.replace('python-','')  if  package_dependency.startswith('python-') else package_dependency
+            package_dependency = (package_dependency.replace('python-', '') if package_dependency.startswith('python-'
+                                  ) else package_dependency)
             execute('pypi_build', package_dependency)
 
