259448a5cef3fed5bef76815bf9654df19ec86b7
Sören König <soeren.koenig@zalando.de>
2014-08-10 17:16:47 +0200

    some cleanup + output formatting

--- create_repository.sh
+++ create_repository.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+## create DEB repo
+# create a file /etc/apt/sources.list.d/zalando.list with following content:
+# deb http://iftp.zalando.net/repo/debian   trusty   zalando OR
+# deb http://iftp.zalando.net/repo/debian   precise  zalando etc.
+
 basedir=/data/zalando/iftp.zalando.net/htdocs/repo/debian/
-
-# repositories=(hardy lucid lenny squeeze)
-# versions(8.0.4 10.0.4 5.0.4 6.0.0)
 repositories=( trusty precise squeeze lenny )
-
 
 for repository in ${repositories[@]}; do
    mkdir -p ${basedir}/pool/${repository}/zalando/binary-i386/
@@ -14,28 +15,22 @@ for repository in ${repositories[@]}; do
 
    find ${basedir}/dists/${repository} -type f -name Packages\* -exec rm {} \;
 
-echo generating ${repository}
-
+   echo -n "generating ${repository} .deb repo..."
 
 ## packages located on one unique location / reference in dists/DISTRIBUTIONNAME/ to this location
 ##################################################################################################
    cd ${basedir} || exit 1
-   dpkg-scanpackages pool/${repository} /dev/null > /tmp/Packages_plain  2> /dev/null
+   dpkg-scanpackages pool/${repository} /dev/null > /tmp/Packages 2> /dev/null
+   cat /tmp/Packages | gzip > /tmp/Packages.gz
+   cat /tmp/Packages | bzip2 > /tmp/Packages.bz2
 
-   cat /tmp/Packages_plain | gzip > /tmp/Packages.gz
-   cat /tmp/Packages_plain | bzip2 > /tmp/Packages.bz2
-
-
-# /data/zalando/iftp.zalando.net/htdocs/repo/debian/pool/lenny/zalando/binary-amd64/contrib/
-# http://iftp.zalando.net/repo/debian/dists/lenny/zalando/binary-amd64/Packages
+   cp /tmp/Packages ${basedir}/dists/${repository}/
+   ln ${basedir}/dists/${repository}/Packages ${basedir}/dists/${repository}/zalando/binary-amd64/Packages
+   ln ${basedir}/dists/${repository}/Packages ${basedir}/dists/${repository}/zalando/binary-i386/Packages
 
    cp /tmp/Packages.gz ${basedir}/dists/${repository}/
    ln ${basedir}/dists/${repository}/Packages.gz ${basedir}/dists/${repository}/zalando/binary-amd64/Packages.gz
    ln ${basedir}/dists/${repository}/Packages.gz ${basedir}/dists/${repository}/zalando/binary-i386/Packages.gz
-
-   cp /tmp/Packages_plain ${basedir}/dists/${repository}/Packages
-   ln ${basedir}/dists/${repository}/Packages ${basedir}/dists/${repository}/zalando/binary-amd64/Packages
-   ln ${basedir}/dists/${repository}/Packages ${basedir}/dists/${repository}/zalando/binary-i386/Packages
 
    cp /tmp/Packages.bz2 ${basedir}/dists/${repository}/
    ln ${basedir}/dists/${repository}/Packages.bz2 ${basedir}/dists/${repository}/zalando/binary-amd64/Packages.bz2
@@ -56,30 +51,14 @@ echo generating ${repository}
 
    echo "signit" | gpg --logger-fd 1 --keyring trustdb.gpg --secret-keyring trustdb.gpg --passphrase-fd 0 -a -b -s -q -u "Zalando Repository <sysop@zalando.de>" --batch -o dists/${repository}/Release.gpg dists/${repository}/Release
 
-   find ${basedir}/dists/${repository}/zalando -type f -name Release  -exec rm {} \;
+   find ${basedir}/dists/${repository}/zalando -type f -name Release -exec rm {} \;
    ln ${basedir}/dists/${repository}/Release dists/${repository}/zalando/binary-amd64/Release
    ln ${basedir}/dists/${repository}/Release dists/${repository}/zalando/binary-i386/Release
+
+   echo "done."
 done
 
 ## create RPM repo
-basedir=/data/zalando/iftp.zalando.net/htdocs/repo/centos/
-
-releases=( 6 )
-sections=( base updates extras )
-archs=( i386 x86_64 )
-
-for release in ${releases[@]}
-do
-    for section in ${sections[@]}
-    do
-        for arch in ${archs[@]}
-        do
-            mkdir -p $basedir/${release}/${section}/${arch}/
-            createrepo $basedir/${release}/${section}/${arch}/
-        done
-    done
-done
-
 # create a file /etc/yum.repos.d/zalando.repo with following content (section names must be uniqe):
 # [zalando-base]
 # name=Zalando-$releasever - Base
@@ -92,3 +71,23 @@ done
 # [zalando-extras]
 # name=Zalando-$releasever - Extras
 # baseurl=http://iftp.zalando.net/repo/centos/$releasever/extras/$basearch/
+
+basedir=/data/zalando/iftp.zalando.net/htdocs/repo/centos/
+releases=( 6 )
+sections=( base updates extras )
+archs=( i386 x86_64 )
+
+for release in ${releases[@]}
+do
+    echo -n "generating CentOS/RedHat ${release} .rpm repo..."
+    for section in ${sections[@]}
+    do
+        for arch in ${archs[@]}
+        do
+            mkdir -p $basedir/${release}/${section}/${arch}/
+            createrepo $basedir/${release}/${section}/${arch}/ &>/dev/null
+        done
+    done
+    echo "done."
+done
+
