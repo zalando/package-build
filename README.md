194527f3b2a23802b61ea241194468b7046cfa7e
Felix Mueller <felix.mueller@zalando.de>
2014-08-18 17:26:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Package build box
+
+## Requirements
+https://www.virtualbox.org/
+http://www.vagrantup.com/
+
+## Setup
+
+    
+
+## How to use
+
---
+++ setup/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install htop vim git-core rubygems
+sudo gem install --no-ri --no-rdoc fpm fpm-cookery
