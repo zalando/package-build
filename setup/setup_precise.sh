12cd398a7799e7601365c6bbd13674e7a14d114e
Sandor Szuecs <sandor.szuecs@zalando.de>
2015-01-08 11:48:25 +0100

    fix setup bash script

--- setup/setup_precise.sh
+++ setup/setup_precise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install git-core curl # fpm-cook dependencies
+sudo apt-get -y install git-core curl ruby-dev # fpm-cook dependencies
 sudo apt-get -y install htop vim rubygems
 sudo gem install --no-ri --no-rdoc fpm fpm-cookery
