43ef05096ab7a7dd07a0b37bba3c76a6a82a4c94
Sören König <soeren.koenig@zalando.de>
2015-01-08 11:46:23 +0100

    back to "only-package-name"

--- fpm-recipes/zalando-tomcat/recipe.rb
+++ fpm-recipes/zalando-tomcat/recipe.rb
@@ -1,10 +1,10 @@
 class ZalandoTomcat < FPM::Cookery::Recipe
   description "Tomcat meta package for Zalando"
 
+  name     "zalando-tomcat"
   version  "7.0.57"
   revision  0
   arch     "all"
-  name     "zalando-tomcat-#{version}"
   homepage "http://tomcat.apache.org/"
   source   "http://archive.apache.org/dist/tomcat/tomcat-7/v#{version}/bin/apache-tomcat-#{version}.tar.gz"
 
