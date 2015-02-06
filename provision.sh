42ae6700ec3b080e1c9c5ed0097464274ff0147c
Sören König <soeren.koenig@zalando.de>
2015-02-06 11:55:32 +0100

    use apt-cacher-ng if possible

--- provision.sh
+++ provision.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if nc -zw 3 repo.zalando 3142
+then
+    cat > /etc/apt/apt.conf.d/01proxy << EOF
+Acquire::http::Proxy "http://repo.zalando:3142";
+Acquire::https::Proxy "https://";
+EOF
+fi
+
 apt-get update
 
 apt-get install -y vim ruby ruby-dev python-setuptools
