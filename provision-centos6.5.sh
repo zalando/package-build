ddb2caa9bb21df6c5e3834b3173fddef6686519e
Sören König <soeren.koenig@zalando.de>
2015-02-11 00:56:31 +0100

    fixed Freudian slip :)

--- provision-centos6.5.sh
+++ provision-centos6.5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install -y ruby ruby-devel ruby-ri ruby-rdoc rubygems python-setuptools git curl vim-common
-gem install --no-rdoc --no-ri fpm fpm-cockery
+gem install --no-rdoc --no-ri fpm fpm-cookery
