814995be6bb804d614fc3c4fb8e34844886e8e07
Sören König <soeren.koenig@zalando.de>
2015-03-06 19:23:58 +0100

    ZOOPORT-12203 cleanup

--- recipes/python3-boto3/recipe.rb
+++ recipes/python3-boto3/recipe.rb
@@ -7,17 +7,15 @@ class Python3Boto3 < FPM::Cookery::Recipe
   homepage 'http://aws.amazon.com/sdk-for-python/'
   source   'https://github.com/boto/boto3.git', :with => :git, :tag => "#{version}"
 
-  build_depends 'python-docutils'
-  #depends       'libsqlite0', 'python-sqlite'
+  build_depends 'python3-setuptools'
 
   def build
-    safesystem 'apt-get install -y python3-setuptools'
     safesystem '/usr/bin/easy_install3 pip'
   end
 
   def install
     with_trueprefix do
-        safesystem "/usr/local/bin/pip3 install boto3 --target #{destdir}/usr/local/lib/python3.2/dist-packages/"
+        safesystem "/usr/local/bin/pip3 install boto3==#{version} --target #{destdir}/usr/local/lib/python3.2/dist-packages/"
     end
   end
 end
