ffa6cd0c73c735c62c7d79bbb51a8a6720eeae83
Sören König <soeren.koenig@zalando.de>
2015-03-09 17:04:07 +0100

    some cleanup in the recipe

--- recipes/python3-boto3/recipe.rb
+++ recipes/python3-boto3/recipe.rb
@@ -1,17 +1,22 @@
+#!/bin/env ruby
+# encoding: utf-8
+#
 class Python3Boto3 < FPM::Cookery::Recipe
-  # This is a bit hacky: boto3 is installed via pip3 into the destdir,
-  # which is going to be packaged by fpm afterwards. So the 'source'
-  # attribute is actually not needed, but syntactically required.
   # This creates the python3-boto3 package with all it's dependencies.
+  # This is a bit hacky: boto3 is installed via pip3 into the destdir,
+  # which is going to be packaged by fpm afterwards.
   description "Python interface to Amazon's Web Services"
 
-  name     'python3-boto3'
-  version  '0.0.10'
+  name     "python3-boto3"
+  version  "0.0.10"
   revision 0
-  homepage 'http://aws.amazon.com/sdk-for-python/'
-  source   'https://github.com/boto/boto3.git', :with => :git, :tag => "#{version}"
+  arch      "all"
+  homepage "http://aws.amazon.com/sdk-for-python/"
+  source   "https://github.com/boto/boto3.git", :with => :git, :tag => "#{version}"
 
-  build_depends 'python3-setuptools'
+  maintainer    "Sören König <soeren.koenig@zalando.de>"
+  build_depends "python3-setuptools"
+  depends 	    "python3"
 
   def build
     safesystem '/usr/bin/easy_install3 pip'
@@ -19,7 +24,7 @@ class Python3Boto3 < FPM::Cookery::Recipe
 
   def install
     with_trueprefix do
-        safesystem "/usr/local/bin/pip3 install boto3==#{version} --target #{destdir}/usr/local/lib/python3.2/dist-packages/"
+        safesystem "/usr/local/bin/pip3 install #{cachedir}/boto3.git --target #{destdir}/usr/local/lib/python3.2/dist-packages/"
     end
   end
 end
