59cc39feea56b8d9ead246a01bfa3907a2a55b4a
Sören König <soeren.koenig@zalando.de>
2015-03-31 15:24:15 +0200

    remove obsolete files

--- provision-centos6.5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yum install -y ruby ruby-devel ruby-ri ruby-rdoc rubygems python-setuptools git curl vim-common
-gem install --no-rdoc --no-ri fpm fpm-cookery
-
-source /vagrant/cook-recipe.sh
