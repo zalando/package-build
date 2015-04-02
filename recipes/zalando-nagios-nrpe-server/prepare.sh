71d27cc4fdd92c02660c3f06579285cecc8172be
Sören König <soeren.koenig@zalando.de>
2015-04-02 16:31:51 +0200

    adapted zalando-nagios-nrpe-* to packaging scripts

---
+++ recipes/zalando-nagios-nrpe-server/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [ -d tmp-build ]
+then
+    cd tmp-build && git pull origin master
+else
+    git clone git://git.code.sf.net/p/nagios/nrpe tmp-build && cd tmp-build
+fi
+
+patch < ../zalando-nagios-nrpe.patch
+./configure --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu --enable-command-args
+cd ..
+make deb
