d35b27caa1b9b48dbaf393e082c6cdeef86d183c
Sören König <soeren.koenig@zalando.de>
2015-04-16 12:10:41 +0200

    remove compiled files after packaging

--- recipes/chef-runner/recipe.rb
+++ recipes/chef-runner/recipe.rb
@@ -32,5 +32,7 @@ class ChefRunner < FPM::Cookery::Recipe
 
   def install
     bin.install builddir("gobuild/bin/chef-runner")
+    rm_rf "#{builddir}/gobuild/src", :verbose => true
+    rm_rf "#{builddir}/gobuild/bin", :verbose => true
   end
 end
