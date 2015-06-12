e6008f45ca150f3c7c9efc49a82428eb343361fc
Sören König <soeren.koenig@zalando.de>
2015-06-12 16:34:51 +0200

    added recipe for "zalando-zcloud"

---
+++ recipes/zalando-zcloud/recipe.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class ZalandoZcloud < FPM::Cookery::Recipe
+  description "Package containing CLI, agent and additional scripts for installing nodes via zCloud"
+
+  name     "zalando-zcloud"
+  version  "0.1.5"
+  revision  201506121630
+  arch      "all"
+
+  homepage      "https://stash.zalando.net/projects/PYMODULES/repos/zalando-zcloud/browse"
+  source        "https://stash.zalando.net/scm/pymodules/zalando-zcloud.git", :with => :git, :tag => "#{version}"
+  maintainer    "Sören König <soeren.koenig@zalando.de>"
+
+  build_depends   "python-setuptools"
+
+  platforms [:ubuntu, :debian] do
+    depends     "zalando-cmdb-client", "python-paramiko >= 1.7.0"
+  end
+
+  platforms [:centos] do
+    depends     "zalando-cmdb-client", "python-paramiko >= 1.7.0"
+  end
+
+  def build
+    safesystem 'python setup.py build'
+  end
+
+  def install
+      safesystem 'python setup.py install --root=../../tmp-dest --no-compile'
+  end
+end
