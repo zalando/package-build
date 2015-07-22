b6be4daff81bf4ac16500543718236e55f465e3b
Sören König <soeren.koenig@zalando.de>
2015-07-22 12:01:26 +0200

    packaging recipe for Appdynamics

---
+++ recipes/zalando-appdynamics-agents/prepare.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+set -x
+
+LONGVERSION=$(sed -n 's|\s*version\s*"\(.*\)"|\1|p' recipe.rb)
+RESOURCES=(
+AppServerAgent.zip
+MachineAgent.zip
+appdynamics-machine.sh
+appdynamics-params.sh
+controller-info.xml
+custom-activity-correlation.xml
+transactions.xml
+)
+
+# build basic directory structure
+[ -d cache/rootfs/server ] || mkdir -p cache/rootfs/server
+[ -d cache/rootfs/usr/local/bin ] || mkdir -p cache/rootfs/usr/local/bin
+
+# download resources
+for RESOURCE in "${RESOURCES[@]}"
+do
+    [ -f cache/$RESOURCE ] || curl -kL --progress-bar -o cache/$RESOURCE https://repo.zalando/static/appdynamics/${RESOURCE}
+done
+
+# put everything in place
+[ -d cache/rootfs/server/appdynamics-agents/appdynamics-jvm ] \
+    || mkdir -p cache/rootfs/server/appdynamics-agents/appdynamics-jvm
+unzip -o cache/AppServerAgent.zip -d cache/rootfs/server/appdynamics-agents/appdynamics-jvm
+
+[ -d cache/rootfs/server/appdynamics-agents/appdynamics-machine ] \
+    || mkdir -p cache/rootfs/server/appdynamics-agents/appdynamics-machine
+unzip -o cache/MachineAgent.zip -d cache/rootfs/server/appdynamics-agents/appdynamics-machine
+
+cp cache/controller-info.xml cache/rootfs/server/appdynamics-agents/appdynamics-jvm/ver${LONGVERSION}/conf/
+cp cache/controller-info.xml cache/rootfs/server/appdynamics-agents/appdynamics-jvm/conf/
+cp cache/custom-activity-correlation.xml cache/rootfs/server/appdynamics-agents/appdynamics-jvm/ver${LONGVERSION}/conf/
+cp cache/transactions.xml cache/rootfs/server/appdynamics-agents/appdynamics-jvm/ver${LONGVERSION}/conf/
+
+cp cache/*sh cache/rootfs/usr/local/bin
+find cache/rootfs -type f -name '*sh' -exec chmod +x {} \;
+
+# The tar will have ./cache/rootfs as top-most structure,
+# but fpm-cookert will skip `cache/` so `root("/").install Dir["rootfs/*"]`
+# creates the .deb content correctly.
+tar caf cache/appdynamics.tgz cache/rootfs/
---
+++ recipes/zalando-appdynamics-agents/recipe.rb
@@ -0,0 +1,24 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class ZalandoAppdynamicsAgents < FPM::Cookery::Recipe
+  description "Appdynamics agents bundle containing AppServerAgent and MachineAgent"
+
+  version   "4.1.0.4"
+  revision   0
+  arch      "all"
+  name      "zalando-appdynamics-agents"
+  homepage  "http://www.appdynamics.com/"
+  source    "cache/appdynamics.tgz"
+
+  maintainer "Sören König <soeren.koenig@zalando.de>"
+  section   "non-free/net"
+
+  def build
+  end
+
+  def install
+     root("/").install Dir["rootfs/*"]
+  end
+
+end
