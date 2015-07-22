def30292dc1a55bb5dbe66cb723ea0774af6e15d
Markus Wyrsch <markus.wyrsch@zalando.de>
2015-07-22 10:56:33 +0200

    add etcd/etcdctl package

--- recipes/etcd/recipe.rb
+++ recipes/etcd/recipe.rb
@@ -1,16 +1,16 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-class Vault < FPM::Cookery::Recipe
-  description "A tool for managing secrets, written in Go."
-  GOPACKAGE = "github.com/hashicorp/vault"
+class Etcd < FPM::Cookery::Recipe
+  description "A distributed consistent key-value store for shared configuration and service discovery"
+  GOPACKAGE = "github.com/coreos/etcd"
 
-  name      "zalando-vault"
-  version   "0.2.1"
-  revision  201507161056
+  name      "zalando-etcd"
+  version   "2.1.1"
+  revision  201507221004
 
-  homepage      "http://vaultproject.io/"
-  source        "https://github.com/hashicorp/vault.git", :with => :git
+  homepage      "https://github.com/coreos/etcd"
+  source        "https://github.com/coreos/etcd.git", :with => :git
   maintainer    "Markus Wyrsch <markus.wyrsch@zalando.de>"
 
   build_depends   "golang-go git"
@@ -28,12 +28,14 @@ class Vault < FPM::Cookery::Recipe
     safesystem "go get -v github.com/gin-gonic/gin/"
     safesystem "cd $GOPATH/src/github.com/gin-gonic/gin/ && git checkout develop"
     safesystem "go get -v #{GOPACKAGE}"
+    safesystem "go get -v #{GOPACKAGE}/etcdctl"
   end
 
   def install
-    etc("init").install_p(workdir("vault.conf.upstart"), "vault.conf")
-    etc("vault").install_p(workdir("vault_config.hcl"), "config.hcl")
-    bin.install builddir("gobuild/bin/vault")
+    etc("init").install_p(workdir("etcd.conf.upstart"), "etcd.conf")
+    etc("init.d").install_p(workdir("etcd_init.d"), "etcd")
+    bin.install builddir("gobuild/bin/etcd")
+    bin.install builddir("gobuild/bin/etcdctl")
     rm_rf "#{builddir}/gobuild/pkg", :verbose => true
     rm_rf "#{builddir}/gobuild/bin", :verbose => true
   end
