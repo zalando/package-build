a00f46f28b9c83a4ae60f1b717cf0a544b1934a1
Markus Wyrsch <markus.wyrsch@zalando.de>
2015-08-24 12:32:58 +0200

    add recipe for prometheus

---
+++ recipes/prometheus/recipe.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class Prometheus < FPM::Cookery::Recipe
+  description "The Prometheus monitoring system and time series database."
+  GOPACKAGE = "github.com/prometheus/prometheus"
+
+  name      "zalando-prometheus"
+  version   "0.15.1"
+  revision  201508241229
+
+  homepage      "http://prometheus.io/"
+  source        "https://github.com/prometheus/prometheus.git", :with => :git
+  maintainer    "Markus Wyrsch <markus.wyrsch@zalando.de>"
+
+  build_depends   "git"
+
+  def build
+      make
+  end
+
+  def install
+    etc("default").install_p(workdir("prometheus.default"), "prometheus")
+    etc("init").install_p(workdir("prometheus.conf.upstart"), "prometheus.conf")
+    bin.install builddir("prometheus-HEAD/prometheus")
+    bin.install builddir("prometheus-HEAD/promtool")
+  end
+end
