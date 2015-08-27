73ff181dbb48b652ce380fd924676a2f3795b15c
Raffaele Di Fazio <raffaele.di.fazio@zalando.de>
2015-08-27 17:27:56 +0200

    Added recipies for kubernetes-podmaster
    Signed-off-by: Raffaele Di Fazio <raffaele.di.fazio@zalando.de>

---
+++ recipes/kubernetes-podmaster/recipe.rb
@@ -0,0 +1,40 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class Podmaster < FPM::Cookery::Recipe
+  description "The podmaster binary for Kubernetes' HA."
+  GOPACKAGE = "github.com/zalando-techmonkeys/kubernetes-podmaster"
+
+  name      "zalando-podmaster"
+  version   "0.0.1"
+  revision  201508271702
+
+  homepage      "https://github.com/zalando-techmonkeys/kubernetes-podmaster"
+  source        "https://github.com/zalando-techmonkeys/kubernetes-podmaster.git", :with => :git
+  maintainer    "Raffaele Di Fazio <raffaele.di.fazio@zalando.de>"
+
+  build_depends   "git"
+
+  def build
+    pkgdir = builddir("gobuild/src/#{GOPACKAGE}")
+    mkdir_p pkgdir
+    cp_r Dir["*"], pkgdir
+
+    ENV["GOPATH"] = builddir("gobuild/")
+    safesystem "go env"
+    safesystem "go get github.com/coreos/go-etcd/etcd"
+    safesystem "go get github.com/golang/glog"
+    safesystem "go get github.com/spf13/pflag"
+    safesystem "go get k8s.io/kubernetes/pkg/storage/etcd"
+    safesystem "cd ${GOPATH}/src/github.com/zalando-techmonkeys/kubernetes-podmaster/"
+    safesystem "go install #{GOPACKAGE}"
+  end
+
+  def install
+    etc("default").install_p(workdir("podmaster.default"), "podmaster")
+    etc("init").install_p(workdir("podmaster.conf.upstart"), "podmaster.conf")
+    bin.install builddir("gobuild/bin/kubernetes-podmaster")
+    rm_rf "#{builddir}/gobuild/pkg", :verbose => true
+    rm_rf "#{builddir}/gobuild/bin", :verbose => true
+  end
+end
