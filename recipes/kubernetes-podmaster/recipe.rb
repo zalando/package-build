7cabbec913cb3eba99571b8a98edeee589267aeb
Markus Wyrsch <markus.wyrsch@zalando.de>
2015-08-28 13:38:44 +0200

    revert changes from last commit, fix names

--- recipes/kubernetes-podmaster/recipe.rb
+++ recipes/kubernetes-podmaster/recipe.rb
@@ -7,7 +7,7 @@ class Podmaster < FPM::Cookery::Recipe
 
   name      "zalando-podmaster"
   version   "0.0.1"
-  revision  201508280956
+  revision  201508281256
 
   homepage      "https://github.com/zalando-techmonkeys/kubernetes-podmaster"
   source        "https://github.com/zalando-techmonkeys/kubernetes-podmaster.git", :with => :git
@@ -31,9 +31,9 @@ class Podmaster < FPM::Cookery::Recipe
   end
 
   def install
-    etc("default").install_p(workdir("podmaster.default"), "podmaster")
-    etc("init").install_p(workdir("podmaster.conf.upstart"), "podmaster.conf")
-    etc("init.d").install_p(workdir("podmaster.init.d"), "podmaster")
+    etc("default").install_p(workdir("kubernetes-podmaster.default"), "kubernetes-podmaster")
+    etc("init").install_p(workdir("kubernetes-podmaster.conf.upstart"), "kubernetes-podmaster.conf")
+    etc("init.d").install_p(workdir("kubernetes-podmaster.init.d"), "kubernetes-podmaster")
     bin.install builddir("gobuild/bin/kubernetes-podmaster")
     rm_rf "#{builddir}/gobuild/pkg", :verbose => true
     rm_rf "#{builddir}/gobuild/bin", :verbose => true
