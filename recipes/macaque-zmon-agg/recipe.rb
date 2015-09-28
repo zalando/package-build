7d806f8f0447c26a4dea2803546594f2872108e1
Sören König <soeren.koenig@zalando.de>
2015-09-28 19:18:28 +0200

    cleaned up build process for macaque, using godep now

--- recipes/macaque-zmon-agg/recipe.rb
+++ recipes/macaque-zmon-agg/recipe.rb
@@ -3,11 +3,11 @@
 
 class MacaqueZmonAgg < FPM::Cookery::Recipe
   description "Macaque is a Zmon aggregator API, written in Go."
-  GOPACKAGE = "macaque-zmon-agg"
+  GOPACKAGE = "github.com/zalando-techmonkeys/macaque-zmon-agg"
 
   name      "macaque-zmon-agg"
-  version   "1.0.0"
-  revision  201509281839
+  version   "1.0.1"
+  revision  201509281912
 
   homepage      "https://github.com/zalando-techmonkeys/macaque-zmon-agg"
   source        "https://github.com/zalando-techmonkeys/macaque-zmon-agg", :with => :git, :tag => "#{version}"
@@ -22,12 +22,17 @@ class MacaqueZmonAgg < FPM::Cookery::Recipe
 
     ENV["GOPATH"] = builddir("gobuild/")
 
-    safesystem "go get -v #{GOPACKAGE}"
+    safesystem "go get github.com/tools/godep"
+    safesystem "cd ${GOPATH}/src/#{GOPACKAGE} && ${GOPATH}/bin/godep restore"
+    safesystem "cd ${GOPATH}/src/#{GOPACKAGE} && go install ./..."
+
   end
 
   def install
     bin.install builddir("gobuild/bin/macaque")
-    #rm_rf "#{builddir}/gobuild/pkg", :verbose => true
-    #rm_rf "#{builddir}/gobuild/bin", :verbose => true
+    (etc/"init.d/").install builddir("gobuild/src/#{GOPACKAGE}/scripts/macaque")
+    (etc/"macaque/").install builddir("gobuild/src/#{GOPACKAGE}/config.yaml.sample")
+    rm_rf "#{builddir}/gobuild/pkg", :verbose => true
+    rm_rf "#{builddir}/gobuild/bin", :verbose => true
   end
 end
