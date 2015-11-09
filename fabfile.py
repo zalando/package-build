f6fa033127f7d027775b5e6729f8158bf2f10b17
Sören König <soeren.koenig@zalando.de>
2015-11-09 17:51:21 +0100

    lots of formatting / normalization

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@
 from __future__ import with_statement
 
 import os
+from os.path import join as pj
 from string import Template
 import json
 import time
@@ -21,11 +22,15 @@ from distutils.util import strtobool
 
 env.disable_known_hosts = True
 
-
 PATH = os.path.dirname(os.path.abspath(__file__))
 RPM_COMPONENTS = ['base', 'updates', 'extras']
 RPM_ARCHS = ['i386', 'x86_64']
-PACKAGE_FORMAT = {'centos6': 'rpm', 'debian7': 'deb', 'ubuntu12.04': 'deb', 'ubuntu14.04': 'deb'}
+PACKAGE_FORMAT = {
+    'centos6': 'rpm',
+    'debian7': 'deb',
+    'ubuntu12.04': 'deb',
+    'ubuntu14.04': 'deb',
+}
 
 CONFIG_FILE = os.path.expanduser('~/.config/package-build.yaml')
 if not os.path.isfile(CONFIG_FILE):
@@ -35,7 +40,7 @@ with open(CONFIG_FILE, 'r') as config_file:
 
 
 def atoi(text):
-    return int(text) if text.isdigit() else text
+    return (int(text) if text.isdigit() else text)
 
 
 def natural_keys(text):
@@ -44,21 +49,23 @@ def natural_keys(text):
     http://nedbatchelder.com/blog/200712/human_sorting.html
     (See Toothy's implementation in the comments)
     '''
+
     return [atoi(c) for c in re.split('(\d+)', text)]
 
 
 def package_exists(path):
     if os.path.isfile(path):
-        print 'package has already been build: {0}'.format(path)
+        print 'package has already been build: {}'.format(path)
         return True
 
     return False
 
+
 def prep_bool_arg(arg):
     return bool(strtobool(str(arg)))
 
-# some repo_* commands must run as root, because sudo won't allow access to the GPG keyring
 
+# some repo_* commands must run as root, because sudo won't allow access to the GPG keyring
 
 @hosts(env.repo_host)
 @with_settings(user='root')
@@ -67,15 +74,15 @@ def repo_rpm_init():
     ''' initialize package repo '''
 
     package_ensure('createrepo')
-    dir_ensure('{0}/archive/{1}'.format(env.repo_rpm_root, dist), recursive=True)
 
     for dist, package_format in PACKAGE_FORMAT.items():
         if package_format == 'rpm':
+            dir_ensure('{0}/archive/{1}'.format(env.repo_rpm_root, dist), recursive=True)
             for component in RPM_COMPONENTS:
                 for arch in RPM_ARCHS:
-                    path = '/'.join([env.repo_rpm_root, dist, component, arch])
+                    path = pj(env.repo_rpm_root, dist, component, arch)
                     dir_ensure(path, recursive=True)
-                    run('createrepo {0}'.format(path))
+                    run('createrepo {}'.format(path))
 
 
 @hosts(env.repo_host)
@@ -95,16 +102,16 @@ def repo_rpm_list(dist='centos6'):
 @with_settings(hide('commands'), user='root')
 @task
 def repo_rpm_add(package, dist='centos6', component='base'):
-    ''' upload and add a package file to the repo '''
+    '''Upload and add package to an apt repo, defaults to centos6'''
 
     arch = 'x86_64'
     if any(map(lambda arch: arch in package, ['i386, i586, i686'])):
         arch = 'i386'
 
-    put(package, '{0}/archive/{1}'.format(env.repo_rpm_root, dist))
+    put(package, pj(env.repo_rpm_root, 'archive', dist))
     package = package.split('/')[-1]
-    path = '/'.join([env.repo_rpm_root, dist, component, arch])
-    run('cp {0}/archive/{1}/{2} {3}'.format(env.repo_rpm_root, dist, package, path))
+    path = pj(env.repo_rpm_root, dist, component, arch)
+    run('cp {0} {1}'.format(pj(env.repo_rpm_root, 'archive', dist, package), path))
     output = run('createrepo {0}'.format(path))
     if output.succeeded:
         print green('added {0} to repo {1}'.format(package, dist))
@@ -144,11 +151,11 @@ def repo_deb_init():
 
     for dist, package_format in PACKAGE_FORMAT.items():
         if package_format == 'deb':
-            dir_ensure('{0}/archive/{1}'.format(env.repo_deb_root, dist), recursive=True)
+            dir_ensure(pj(env.repo_deb_root, 'archive', dist), recursive=True)
             configfile = 'aptly-{0}.conf'.format(dist)
             with open(configfile, 'w') as fh:
-                    content = template.safe_substitute(repo_root=env.repo_deb_root, release=dist)
-                    fh.write(content)
+                content = template.safe_substitute(repo_root=env.repo_deb_root, release=dist)
+                fh.write(content)
             put(configfile, '/etc/aptly-{0}.conf'.format(dist))
             os.unlink(configfile)
             run('/usr/bin/aptly -config=/etc/aptly-{0}.conf repo list --raw=true | grep -q {0} \
@@ -167,22 +174,33 @@ def republish(dist='ubuntu14.04', snapshot=False):
     with hide('commands'):
         if snapshot:
             print 'drop current publication of repo {0}, if existing'.format(green(dist))
-            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all publish drop {0}'.format(dist), warn_only=True)
+            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all \
+                   publish drop {0}'.format(dist),
+                warn_only=True)
 
             print 'drop snapshot from today, if existing'
-            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all snapshot drop $(date +%F)'.format(dist), warn_only=True)
+            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all \
+                    snapshot drop $(date +%F)'.format(dist),
+                warn_only=True)
 
             print 'create current snapshot'
-            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all snapshot create $(date +%F) from repo {0}'.format(dist)).failed:
+            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all \
+                    snapshot create $(date +%F) from repo {0}'.format(dist)).failed:
                 return False
 
             print 'publish current snapshot'
-            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all publish snapshot $(date +%F)'.format(dist)).failed:
+            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf -architectures=i386,amd64,all \
+                    publish snapshot $(date +%F)'.format(dist)).failed:
                 return False
         else:
-            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf publish list -raw=true | grep -q {0} || \
-                 /usr/bin/aptly -config=/etc/aptly-{0}.conf publish repo -distribution={0} {0}'.format(dist), warn_only=True)
-            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf publish update -force-overwrite {0}'.format(dist), warn_only=True).failed:
+            run('/usr/bin/aptly -config=/etc/aptly-{0}.conf \
+                    publish list -raw=true | grep -q {0} || \
+                 /usr/bin/aptly -config=/etc/aptly-{0}.conf \
+                 publish repo -distribution={0} {0}'.format(dist),
+                warn_only=True)
+            if run('/usr/bin/aptly -config=/etc/aptly-{0}.conf \
+                    publish update -force-overwrite {0}'.format(dist),
+                   warn_only=True).failed:
                 return False
 
     return True
@@ -207,18 +225,20 @@ def repo_deb_list(dist='ubuntu14.04', snapshot=False):
 @with_settings(user='root')
 @task
 def repo_deb_add(package, dist='ubuntu14.04'):
-    '''Upload and add package to an apt repo with given dist, defaults to ubuntu14.04
+    '''Upload and add package to an apt repo, defaults to ubuntu14.04
 
     Example:
     % fab repo_deb_add:myfoo.deb,dist=ubuntu14.04
     '''
+
     if not os.path.isfile(os.path.expanduser(package)):
         abort('could not upload {0}: file not found'.format(package))
 
     with hide('commands'):
         put(package, '{0}/archive/{1}'.format(env.repo_deb_root, dist))
         package = package.split('/')[-1]
-        run('/usr/bin/aptly -config=/etc/aptly-{0}.conf repo add -force-replace {0} {1}/archive/{0}/{2}'.format(dist, env.repo_deb_root,  package))
+        run('/usr/bin/aptly -config=/etc/aptly-{0}.conf repo add -force-replace {0} {1}/archive/{0}/{2}'.format(dist,
+            env.repo_deb_root, package))
 
     if republish(dist):
         print green('added {0} to repo {1}'.format(package, dist))
@@ -248,7 +268,9 @@ def repo_deb_rebuild(dist='ubuntu14.04'):
     run('find {0}/archive/{1}/ -name "*.deb" \
             | sort -n \
             | while read package; do dpkg -I $package >/dev/null \
-            && /usr/bin/aptly -config=/etc/aptly-{1}.conf repo add -force-replace {1} $package; done'.format(env.repo_deb_root, dist))
+            && /usr/bin/aptly -config=/etc/aptly-{1}.conf \
+            repo add -force-replace {1} $package; done'.format(env.repo_deb_root,
+        dist))
 
     if republish(dist):
         print green('successfully rebuild repo for dist {0}'.format(dist))
@@ -258,6 +280,7 @@ def repo_deb_rebuild(dist='ubuntu14.04'):
 @with_settings(hide('commands'))
 def package_info(package):
     ''' display metadata info for package file'''
+
     info = {
         'version': None,
         'description': None,
@@ -282,7 +305,8 @@ def package_info(package):
 
     if package.endswith('.rpm'):
         info['version'] = local('rpm -qp --dbpath=/tmp --queryformat=%{{VERSION}} {0}'.format(package), capture=True)
-        info['description'] = local('rpm -qp --dbpath=/tmp --queryformat=%{{SUMMARY}} {0}'.format(package), capture=True)
+        info['description'] = local('rpm -qp --dbpath=/tmp --queryformat=%{{SUMMARY}} {0}'.format(package),
+                                    capture=True)
         info['arch'] = local('rpm -qp --dbpath=/tmp --queryformat=%{{ARCH}} {0}'.format(package), capture=True)
         info['release'] = local('rpm -qp --dbpath=/tmp --queryformat=%{{RELEASE}} {0}'.format(package), capture=True)
 
@@ -297,6 +321,7 @@ def package_info(package):
 @task
 def docker_build(dist=None, force=False):
     ''' build Docker image from ./docker/<dist>/Dockerfile '''
+
     force = prep_bool_arg(force)
 
     def build(dist, force):
@@ -306,18 +331,18 @@ def docker_build(dist=None, force=False):
                 image_existing = local('docker images | grep -q package_build/{dist}'.format(dist=dist)).succeeded
 
         if not image_existing:
-            print('(re)building image {}...'.format(dist))
+            print '(re)building image {}...'.format(dist)
             local('docker build --tag=package_build/{dist} {path}/docker/{dist}/'.format(dist=dist, path=PATH))
 
     if dist:
-        print('building Docker image for {}...'.format(dist))
-        builddir = os.path.join(PATH, 'docker', dist)
+        print 'building Docker image for {}...'.format(dist)
+        builddir = pj(PATH, 'docker', dist)
         os.path.isdir(builddir) or abort('{} dir is not existing'.format(builddir))
         build(dist, force)
     else:
-        print('building Docker images for all distributions...')
-        for entry in os.listdir(os.path.join(PATH, 'docker')):
-            if os.path.isdir(os.path.join(PATH, 'docker', entry)):
+        print 'building Docker images for all distributions...'
+        for entry in os.listdir(pj(PATH, 'docker')):
+            if os.path.isdir(pj(PATH, 'docker', entry)):
                 build(entry, force)
 
 
@@ -345,19 +370,18 @@ def package_build(dist=None, recipe='', upload=False):
     ''' build packages from recipe for dist '''
 
     start_time = time.time()
-    print 'start_time: %s' % start_time
 
     execute('docker_run', dist, '/data/cook-recipe.sh {}'.format(recipe))
 
-    for root, dirs, files in os.walk('./recipes/'):
+    for root, dirs, files in os.walk(pj(PATH, 'recipes')):
         for file in files:
-            if file == 'lastbuild' and os.path.getmtime(os.path.join(root, file)) >= start_time:
+            if file == 'lastbuild' and os.path.getmtime(pj(root, file)) >= start_time:
                 package_name = ''
-                with open(os.path.join(root, file), 'r') as fh:
+                with open(pj(root, file), 'r') as fh:
                     package_name = fh.readline().strip()
                 if package_name:
                     package_format = package_name.split('.')[-1]
                     dist = root.split('/')[-1]
                     if upload:
-                        execute('repo_{0}_add'.format(package_format), os.path.join(root, package_name), dist)
+                        execute('repo_{0}_add'.format(package_format), pj(root, package_name), dist)
     print 'task ran {0} seconds'.format(time.time() - start_time)
