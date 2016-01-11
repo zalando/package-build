8ba89d96e671cbcc0ce0f561046a40be374c54d3
Sören König <soeren.koenig@zalando.de>
2016-01-11 15:35:21 +0100

    build package from version 1.0.24

--- recipes/zalando-cmdb-client/recipe.rb
+++ recipes/zalando-cmdb-client/recipe.rb
@@ -5,7 +5,7 @@ class ZalandoCMDBClient < FPM::Cookery::Recipe
   description "Python client library for CMDB REST API."
 
   name     "zalando-cmdb-client"
-  version  "1.0.23"
+  version  "1.0.24"
   revision  201504071657
   arch     "all"
 
