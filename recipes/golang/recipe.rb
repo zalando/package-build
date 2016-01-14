3d9f8bb1f6695d5161de3cbb2445c5a5d98772ed
Sören König <soeren.koenig@zalando.de>
2016-01-14 09:51:31 +0100

    updated golang to 1.5.3

--- recipes/golang/recipe.rb
+++ recipes/golang/recipe.rb
@@ -2,10 +2,10 @@ class Golang < FPM::Cookery::Recipe
   description 'golang'
 
   name      'golang-go'
-  version   '2:1.5.1'
+  version   '1.5.3'
   homepage  'http://golang.org/'
-  source    "http://golang.org/dl/go1.5.1.linux-amd64.tar.gz"
-  sha1      '46eecd290d8803887dec718c691cc243f2175fe0'
+  source    "http://golang.org/dl/go#{version}.linux-amd64.tar.gz"
+  sha256    '43afe0c5017e502630b1aea4d44b8a7f059bf60d7f29dfd58db454d4e4e0ae53'
 
   config_files '/etc/profile.d/go.sh'
 
