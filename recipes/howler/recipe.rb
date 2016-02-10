1b2ef580ba6315c4c406a090da7b3da3422b6678
Raffaele Di Fazio <raffaele.di.fazio@zalando.de>
2016-02-10 17:54:25 +0100

    New howler version

--- recipes/howler/recipe.rb
+++ recipes/howler/recipe.rb
@@ -6,8 +6,8 @@ class Howler < FPM::Cookery::Recipe
   GOPACKAGE = "github.com/zalando-techmonkeys/howler"
 
   name      "howler"
-  version   "0.0.5"
-  revision  201601221100
+  version   "0.0.7"
+  revision  201602091626
 
   homepage      "https://github.com/zalando-techmonkeys/howler"
   source        "https://github.com/zalando-techmonkeys/howler", :with => :git, :tag => "#{version}"
