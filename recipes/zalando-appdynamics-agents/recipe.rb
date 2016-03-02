96dab57704c483778d4ef20583dd26d29b3fcf9b
Sören König <soeren.koenig@zalando.de>
2016-03-02 16:00:28 +0100

    build revision 2 of zalando-appdynamics-agents 4.1.0.4

--- recipes/zalando-appdynamics-agents/recipe.rb
+++ recipes/zalando-appdynamics-agents/recipe.rb
@@ -5,7 +5,7 @@ class ZalandoAppdynamicsAgents < FPM::Cookery::Recipe
   description "Appdynamics agents bundle containing AppServerAgent and MachineAgent"
 
   version   "4.1.0.4"
-  revision   1
+  revision   2
   arch      "all"
   name      "zalando-appdynamics-agents"
   homepage  "http://www.appdynamics.com/"
