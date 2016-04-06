c024cc408de529e97ca98acf1e803f118199b0e4
Felix Mueller <felix.mueller@zalando.de>
2016-04-06 09:56:56 +0200

    Increment AppDynamcis version

--- recipes/zalando-appdynamics-agents/recipe.rb
+++ recipes/zalando-appdynamics-agents/recipe.rb
@@ -5,7 +5,7 @@ class ZalandoAppdynamicsAgents < FPM::Cookery::Recipe
   description "Appdynamics agents bundle containing AppServerAgent and MachineAgent"
 
   version   "4.1.0.4"
-  revision   3
+  revision   4
   arch      "all"
   name      "zalando-appdynamics-agents"
   homepage  "http://www.appdynamics.com/"
