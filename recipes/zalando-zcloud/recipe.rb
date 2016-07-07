86bac95b5203d3ec114e1d2e24bc878e71001e70
Sören König <soeren.koenig@zalando.de>
2016-07-07 16:20:20 +0200

    build "zalando-zcloud" from version 0.2.13

--- recipes/zalando-zcloud/recipe.rb
+++ recipes/zalando-zcloud/recipe.rb
@@ -5,8 +5,8 @@ class ZalandoZcloud < FPM::Cookery::Recipe
   description "Package containing CLI, agent and additional scripts for installing nodes via zCloud"
 
   name     "zalando-zcloud"
-  version  "0.2.12"
-  revision  201605301337
+  version  "0.2.13"
+  revision  201607061508
   arch     "all"
 
   homepage      "https://stash.zalando.net/projects/PYMODULES/repos/zalando-zcloud/browse"
