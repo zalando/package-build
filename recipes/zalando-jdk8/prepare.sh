f02e039d3f8748dae38de130afb19c072dffb353
Sören König <soeren.koenig@zalando.de>
2017-07-26 17:22:53 +0200

    upgrade JDK to 8u131

--- recipes/zalando-jdk8/prepare.sh
+++ recipes/zalando-jdk8/prepare.sh
@@ -7,7 +7,18 @@ VERSION=${LONGVERSION##*_}
 JDK_TARGET="cache/jdk-8u${VERSION}-linux-x64.tar.gz"
 JCE_TARGET="cache/jce_policy-8.zip"
 
+# Oracle keeps changing their download links, see this thread: https://stackoverflow.com/questions/10268583/downloading-java-jdk-on-linux-via-wget-is-shown-license-page-instead#
+case ${LONGVERSION} in
+    '1.8.0_131')
+        JDK_SOURCE='http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
+        ;;
+    *)
+        echo >&2 "there is no JDK_SOURCE configured for ${LONGVERSION}"
+        exit 1
+        ;;
+esac
+
 [ -d cache ] || mkdir -p cache
-[ -f $JDK_TARGET ] || curl -jkLH 'Cookie: oraclelicense=accept-securebackup-cookie' --progress-bar -o $JDK_TARGET http://download.oracle.com/otn-pub/java/jdk/8u${VERSION}-b15/jdk-8u${VERSION}-linux-x64.tar.gz
+[ -f $JDK_TARGET ] || curl -jkLH 'Cookie: oraclelicense=accept-securebackup-cookie' --progress-bar -o $JDK_TARGET $JDK_SOURCE
 [ -f $JCE_TARGET ] || curl -jkLH 'Cookie: oraclelicense=accept-securebackup-cookie' --progress-bar -o $JCE_TARGET http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
 unzip -o cache/jce_policy-8.zip -d cache/
--- recipes/zalando-jdk8/recipe.rb
+++ recipes/zalando-jdk8/recipe.rb
@@ -4,14 +4,14 @@
 class ZalandoJDK8 < FPM::Cookery::Recipe
   description "Tomcat meta package for Zalando"
 
-  version   "1.8.0-112"
+  version   "1.8.0-131"
   revision   0
   arch      "all"
   name      "zalando-jdk8-#{version}"
   homepage  "http://www.oracle.com/"
   source    "cache/jdk-8u#{version.split('-')[-1]}-linux-x64.tar.gz"
-  # get checksums from https://www.oracle.com/webfolder/s/digest/8u112checksum.html
-  sha256    "777bd7d5268408a5a94f5e366c2e43e720c6ce4fe8c59d9a71e2961e50d774a5"
+  # get checksums from https://www.oracle.com/webfolder/s/digest/8u#{version.split('-')[-1]}checksum.html
+  sha256    "62b215bdfb48bace523723cdbb2157c665e6a25429c73828a32f00e587301236"
 
   maintainer "Sören König <soeren.koenig@zalando.de>"
   section   "non-free/net"
